a93bb85adef92e752289519951d3ebd3c4b54f85
matveyadamey <71337113+matveyadamey@users.noreply.github.com>
2023-01-10 20:02:48 +0300

    Initial commit

---
+++ Assets/Scripts/CameraControlle.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class CameraControlle : MonoBehaviour
+{
+    [SerializeField] Vector3 offset;
+    [SerializeField] Transform player;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.position + offset;
+    }
+}
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Movement : MonoBehaviour
+{
+    Rigidbody rb;
+    float x;
+    float z;
+    [SerializeField]float speed;
+    bool IsGround;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (IsGround)
+        {
+            print(transform.forward);
+            x = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+            z = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+            rb.velocity = new Vector3(x, 0, z);
+            
+        }
+    }
+    private void OnCollisionStay(Collision collision)
+    {
+        IsGround = true;
+        if (collision.gameObject.tag == "deadZone")
+        {
+            SceneManager.LoadScene("level1");
+        }
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        IsGround = false;
+    }
+}
