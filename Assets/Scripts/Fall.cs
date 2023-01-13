68c93b8142f6f9d9887b0610bbb14cc27028188c
matveyadamey <71337113+matveyadamey@users.noreply.github.com>
2023-01-13 20:09:29 +0300

    many many

---
+++ Assets/Scripts/Fall.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Fall : MonoBehaviour
+{
+    Rigidbody rb;
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        StartCoroutine(fall());
+    }
+    IEnumerator fall()
+    {
+        yield return new WaitForSeconds(1);
+        rb.isKinematic = false;
+        rb.AddForce(Vector3.down);
+    }
+}
--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -3,36 +3,44 @@ using UnityEngine.SceneManagement;
 public class Movement : MonoBehaviour
 {
     Rigidbody rb;
-    float x;
-    float z;
-    [SerializeField]float speed;
+    float x,z;
+    [SerializeField] float speed;
     bool IsGround;
 
-    // Start is called before the first frame update
+
     void Start()
     {
         rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
+
     void Update()
     {
         if (IsGround)
         {
-            print(transform.forward);
-            x = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
-            z = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+            x = -Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+            z = -Input.GetAxis("Vertical") * speed * Time.deltaTime;
             rb.velocity = new Vector3(x, 0, z);
             
         }
+        
     }
     private void OnCollisionStay(Collision collision)
     {
         IsGround = true;
+
+
+        if (collision.gameObject.tag == "winZone")
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
+        }
+
+
         if (collision.gameObject.tag == "deadZone")
         {
-            SceneManager.LoadScene("level1");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         }
+
     }
     private void OnCollisionExit(Collision collision)
     {
